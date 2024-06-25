def64b0e168696c1ce8e15363f6e1ad19d2ff0fe
jensimaniya <jensimaniya@gmail.com>
2024-06-25 16:45:10 +0530

    ch-2

---
+++ ch-2/AGE.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   float b,h;
+   clrscr();
+   printf("enter b:");
+   scanf("%f",&b);
+   printf("enter h:");
+   scanf("%f",&h);
+   printf("area of triangle=%f",0.5*b*h);
+   getch();
+}
